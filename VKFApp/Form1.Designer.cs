19df457a7ca4a4c5ef4608dc6aa913eb3c2b8b94
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2021-12-28 23:37:29 +0400

    Ajoutez des fichiers projet.

---
+++ VKFApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace VKFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.accueilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.basesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.traitementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boitesÀOutilsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paramètresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accueilToolStripMenuItem,
+            this.gestionsToolStripMenuItem,
+            this.basesToolStripMenuItem,
+            this.traitementToolStripMenuItem,
+            this.boitesÀOutilsToolStripMenuItem,
+            this.paramètresToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // accueilToolStripMenuItem
+            // 
+            this.accueilToolStripMenuItem.Name = "accueilToolStripMenuItem";
+            this.accueilToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.accueilToolStripMenuItem.Text = "&Accueil";
+            // 
+            // gestionsToolStripMenuItem
+            // 
+            this.gestionsToolStripMenuItem.Name = "gestionsToolStripMenuItem";
+            this.gestionsToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.gestionsToolStripMenuItem.Text = "&Gestions";
+            // 
+            // basesToolStripMenuItem
+            // 
+            this.basesToolStripMenuItem.Name = "basesToolStripMenuItem";
+            this.basesToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.basesToolStripMenuItem.Text = "&Bases";
+            // 
+            // traitementToolStripMenuItem
+            // 
+            this.traitementToolStripMenuItem.Name = "traitementToolStripMenuItem";
+            this.traitementToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.traitementToolStripMenuItem.Text = "&Traitement";
+            // 
+            // boitesÀOutilsToolStripMenuItem
+            // 
+            this.boitesÀOutilsToolStripMenuItem.Name = "boitesÀOutilsToolStripMenuItem";
+            this.boitesÀOutilsToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.boitesÀOutilsToolStripMenuItem.Text = "&Boites à outils";
+            // 
+            // paramètresToolStripMenuItem
+            // 
+            this.paramètresToolStripMenuItem.Name = "paramètresToolStripMenuItem";
+            this.paramètresToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.paramètresToolStripMenuItem.Text = "&Paramètres";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "&?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "VKFApp";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem accueilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem basesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem traitementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boitesÀOutilsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paramètresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
